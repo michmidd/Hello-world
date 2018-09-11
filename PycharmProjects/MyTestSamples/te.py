83084dad97188ee5bd0142b6338a06b2e0200ce6
michmidd <mich1midd@yahoo.com>
2018-09-11 15:38:33 -0400

    First file

---
+++ PycharmProjects/MyTestSamples/te.py
@@ -0,0 +1,15 @@
+#update from Michelle M.
+host = "https://test-prod-us-west-2.rapidsos.com"
+
+
+print(host)
+print(host.upper())
+print(host.lower())
+print(host.title())
+
+list1 = host
+
+list1 = host.split("-")
+print(list1)
+
+print("-".join(list1))
