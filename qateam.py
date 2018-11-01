6dd83d6aae9299fbbf36e5dbf144eda1bda2fa62
michmidd <mich1midd@yahoo.com>
2018-10-31 21:09:01 -0400

    Add files via upload

---
+++ qateam.py
@@ -0,0 +1,15 @@
+#This script utilizes the list feature.
+
+print("RapidSOS QA Team Members\n")
+
+team_member1 = "Gabriel:\t\t", "Senior QA Automation Engineer"
+team_member2 = "Isaac:\t\t", "Senior QA Engineerr"
+team_member3 = "Brian:\t\t", "QA Team Lead"
+team_member4 = "Michelle:\t\t", "QA Analyst"
+team_member5 = "Syed:\t\t", "Senior QA Performance Engineer"
+ 
+print(' Gabriel:\t', 'Senior QA Automation Engineer\n', 'Isaac:\t\t', 'Senior QA Engineer\n', 'Brian:\t\t', 'QA Team Lead\n', 'Michelle:\t', 'QA Analyst\n', 'Syed:\t\t', 'Senior QA Perrformance Engineer')
+print("\nStay Tuned....Continued Growth!")
+
+
+    
