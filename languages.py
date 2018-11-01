8e613ba606959eec35b423b433bd781dd6d6354a
michmidd <mich1midd@yahoo.com>
2018-10-31 21:07:57 -0400

    Add files via upload

---
+++ languages.py
@@ -0,0 +1,9 @@
+
+favorite_languages = {
+    'Ben': 'python',
+    'Brad': 'Ruby',
+    'John': 'C++',
+    }
+
+for name, language in favorite_languages.items():
+    print(name.title() + "'s favorite languages is " + language.title() + ".")
