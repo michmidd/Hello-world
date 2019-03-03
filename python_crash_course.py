35f35be88ae2e8e8db555d13060eb6b1db9b895e
michmidd <mich1midd@yahoo.com>
2019-03-03 12:37:45 -0500

    Add files via upload

---
+++ python_crash_course.py
@@ -0,0 +1,83 @@
+#This is just a test
+
+print('Hello Python World!')
+message = 'Hello Python World'
+print(message)
+message = 'Hello Python Crash Course World!'
+print(message)
+message ='Hello Python Crash Course Reader!'
+print(message)
+
+
+#Strings
+
+name = 'ada lovelace'
+print(name.title())             #title method
+
+first_name = 'ada'
+last_name = 'lovelace'
+full_name = first_name + " " + last_name
+print(full_name)
+print("Hello, "+ full_name.title() + "!")
+
+
+name = 'ada lovelace'
+print(name)
+print(name.title())             #title method
+
+
+message = "Hello, "+ full_name.title() + "!"
+print(message)
+
+#Print a name in lower and upper case
+print(name.lower())
+print(name.upper())
+
+
+#Print strings on a new line and tab
+print('Python')
+print('\tPython')
+print("languages:\nPython\nC\nJavaScript")
+print("languages:\n\tPython\n\tC\n\tJavaScript")
+
+
+favorite_language = ' python '
+favorite_language = favorite_language.strip()
+print(favorite_language)
+
+
+#Practice exercises
+
+name_1 = 'Benjamin'
+message = ("Hello, " + name_1.title() + " would you like to learn some Python today? ")
+print(message)
+
+print(name_1.lower())
+print(name_1.upper())
+print(name_1.title())
+
+famous_quote = 'Albert Einstein once said, "A person who never made a mistake never tried anything new."'
+print(famous_quote)
+
+famous_person = 'Albert Einstein'
+message = famous_person + ' once said, "A person who never made a mistake never tried anything new."'
+print(message)
+
+name2 = ' Brad '
+print(name2.lstrip())
+print(name2.rstrip())
+print(name2.strip())
+
+
+
+#Numbers
+
+
+
+
+
+
+
+
+
+ 
